680f32ebece99631b77724dfdd9a4e63e46527b7
berbiche <berbiche@gmail.com>
2020-11-18 13:51:53 +0100

    mise en place première route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
 import "./App.css";
 import settingsIcon from "./assets/settings.svg";
 import hamburgerIcon from "./assets/reorder-three-outline.svg";
@@ -17,21 +19,32 @@ export const App = () => {
   };
 
   return (
-    <Wrapper className="App">
+    <BrowserRouter className="App">
       <header className="App-header">
         <img src={hamburgerIcon} className="hamburger-icon" alt="menu" />
         {name && password ? <h2>Login réussi, bonjour {name}!</h2> : null}
         <img src={settingsIcon} className="settings-icon" alt="settings" />
       </header>
+
       <div className="main-container">
-        <div className="sidebar"></div>
+        <div className="sidebar">
+          <ul>
+            <li className="menu-entry">Login (accès à contenu privé)</li>
+            <li className="menu-entry">Usage de state 1</li>
+            <li className="menu-entry">Usage de state 2</li>
+            <li className="menu-entry">Génération de divs</li>
+          </ul>
+        </div>
         <div className="main-column">
           <ExoLogin sendBackData={loginHandler}></ExoLogin>
-          <ExoCompteur initialValue={0} incrValue={+1} decrValue={-1} />
+          <Route path="/compteur1">
+            <ExoCompteur initialValue={0} incrValue={+1} decrValue={-1} />
+          </Route>
+
           <ExoCompteur initialValue={10} incrValue={+10} decrValue={-10} />
           <ExoCouleur></ExoCouleur>
         </div>
       </div>
-    </Wrapper>
+    </BrowserRouter>
   );
 };
